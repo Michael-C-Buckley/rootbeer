cf29333e4f23776cc77d62e4d0d3484a3206ef17
Michael Buckley <michaelcbuckley@proton.me>
2026-07-24 07:25:35 -0400

    Nushell: tweaks to make it more useful

--- modules/shells/nushell.lua
+++ modules/shells/nushell.lua
@@ -6,7 +6,6 @@ optional.link_files_when_binary("nu", {
   { dir .. "/env.nu", "~/.config/nushell/env.nu" },
   { dir .. "/config.nu", "~/.config/nushell/config.nu" },
   { dir .. "/abbreviations.nu", "~/.config/nushell/abbreviations.nu" },
-  { dir .. "/git.nu", "~/.config/nushell/git.nu" },
   { dir .. "/prompt.nu", "~/.config/nushell/prompt.nu" },
   { dir .. "/zoxide.nu", "~/.config/nushell/zoxide.nu" },
 })
