9b5344a1f325ac7095646111062c55ef93968655
Michael Buckley <michaelcbuckley@proton.me>
2026-07-30 10:17:09 -0400

    Ghostty: changed the config to be pre-rendered files for external consumption

--- modules/terminals/ghostty.lua
+++ modules/terminals/ghostty.lua
@@ -1,27 +1,7 @@
+local optional = require("modules.lib.optional")
 local rb = require("rootbeer")
 
-local config = [[
-cursor-color = #44A3A3
-cursor-opacity = 0.6
-font-family = Lilex Nerd Font Mono
-background = #000000
-keybind = performable:ctrl+shift+h=previous_tab
-keybind = performable:ctrl+shift+l=next_tab
-tab-inherit-working-directory = false
-working-directory = home
-]]
-
-if rb.host.os == "macos" then
-  config = config
-    .. [[
-font-size = 14
-font-thicken = true
-font-thicken-strength = 255
-]]
-else
-  config = config .. [[
-font-size = 11
-]]
-end
-
-rb.file("~/.config/ghostty/config", config)
+local dir = "configs/terminal/ghostty/"
+optional.link_files_when_binary("ghostty", {
+  { dir .. rb.host.os, "~/.config/ghostty/config" },
+})
