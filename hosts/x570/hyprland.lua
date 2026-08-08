7af15a810b9e955ce8b8459088cd3762c65e5105
Michael Buckley <michaelcbuckley@proton.me>
2026-08-08 15:35:51 -0400

    X570 moves to one monitor

--- hosts/x570/hyprland.lua
+++ hosts/x570/hyprland.lua
@@ -7,18 +7,6 @@ hl.monitor({
   scale = 1,
 })
 
-hl.monitor({
-  output = "HDMI-A-3",
-  mode = "2560x1440@74.60",
-  position = "3440x-500",
-  scale = 1,
-  transform = 3,
-})
-
-for i = 1, 9 do
+for i = 1, 10 do
   hl.workspace_rule({ workspace = tostring(i), monitor = "DP-1" })
-end
-
-for i = 9, 11 do
-  hl.workspace_rule({ workspace = tostring(i), monitor = "HDMI-A-3" })
 end
