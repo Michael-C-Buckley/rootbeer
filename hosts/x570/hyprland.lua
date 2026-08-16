4216b1ce5524778b5c6243933360afca7da67706
Michael Buckley <michaelcbuckley@proton.me>
2026-08-16 08:42:31 -0400

    Binds change

--- hosts/x570/hyprland.lua
+++ hosts/x570/hyprland.lua
@@ -2,11 +2,6 @@
 
 hl.monitor({
   output = "DP-1",
-  mode = "3440x1440@144.00",
-  position = "0x0",
+  mode = "3440x1440@165.00",
   scale = 1,
 })
-
-for i = 1, 10 do
-  hl.workspace_rule({ workspace = tostring(i), monitor = "DP-1" })
-end
