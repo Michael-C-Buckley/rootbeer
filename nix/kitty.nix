ebd160e48fe6c552855d193a2f561d42011c905e
Michael Buckley <michaelcbuckley@proton.me>
2026-08-18 08:03:42 -0400

    Kitty: added basic attrs as override anchors

--- nix/kitty.nix
+++ nix/kitty.nix
@@ -1,7 +1,13 @@
-{pkgs, ...}: let
-  font = pkgs.makeFontsConf {fontDirectories = [pkgs.lilex];};
+{
+  pkgs,
+  extraConf ? '''',
+  fonts ? [pkgs.lilex],
+  paths ? [pkgs.kitty],
+  ...
+}: let
+  font = pkgs.makeFontsConf {fontDirectories = fonts;};
   config = ../configs/terminal/kitty/kitty.conf;
-  # Shim to write options until I declare an override
+  # My default, unless I override
   optionsFile = ''
     font_family family='Lilex Nerd Font' style=Medium
     font_size 11
@@ -9,14 +15,17 @@
 in
   pkgs.symlinkJoin {
     name = "kitty";
-    paths = [pkgs.kitty];
+    inherit paths;
     nativeBuildInputs = [pkgs.makeWrapper];
     postBuild = ''
       ln -s ${config} $out/kitty.conf
 
       mkdir -p "$out/kitty.d/nix"
       printf '%s' ${pkgs.lib.escapeShellArg optionsFile} \
-        > "$out/kitty.d/nix/99_options.conf"
+        > "$out/kitty.d/nix/98_nix_default.conf"
+
+      printf '%s' ${pkgs.lib.escapeShellArg extraConf} \
+        > "$out/kitty.d/nix/99_extra.conf"
 
       wrapProgram $out/bin/kitty \
         --set FONTCONFIG_FILE ${font} \
